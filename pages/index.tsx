f0a1dfa82d4be92e54c6938201e85f9506b9c7bd
Gfreschi <gabrielfreschi@outlook.com>
2022-04-07 21:58:51 -0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>HOMEPAGE</title>
+      </Head>
+
+      <main>
+        <h1>HELLO WORLD</h1>
+      </main>
+    </div>
+  )
+}
