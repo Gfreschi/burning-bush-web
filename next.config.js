b386cc9931ac4252b6e1747f6b9babe4eb750a68
Gfreschi <gabrielfreschi@outlook.com>
2022-04-08 11:09:16 -0300

    styled-components initial config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
 module.exports = nextConfig
+
+const withImages = require('next-images')
+
+module.exports = withImages({
+  esModule: true,
+})
+
