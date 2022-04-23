986edb401b9ef20960328130543293d2a183e9a7
Gfreschi <gabrielfreschi@outlook.com>
2022-04-23 12:52:27 -0300

    feat: mui example and removed styled-components

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,14 +1,24 @@
-const theme = {
-  colors: {
-    background: '#3faa8a',
-    text: '#e1e1e6',
-    primary: '#8257e6'
-  }
-}
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
 
 const landing = {
   colors: {
-    
+
   }
 }
 export default theme
