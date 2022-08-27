18d65532a9f98f82bc8a645bb38b993b1912fdb3
Gfreschi <gabrielfreschi@outlook.com>
2022-08-27 16:25:43 -0300

    feat: map refinement and new features

--- src/components/WorldMapBox/index.tsx
+++ src/components/WorldMapBox/index.tsx
@@ -1,77 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import ReactMapboxGl, { Feature, Layer, Marker, Popup } from 'react-mapbox-gl'
+import * as React from 'react'
+import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { Box } from '@material-ui/core'
 
-type Complaint = {
-  id: number
-  latitude: number
-  longitude: number
+interface MapboxMapProps {
+  initialOptions?: Omit<mapboxgl.MapboxOptions, 'container'>
+  onCreated?(map: mapboxgl.Map): void
+  onLoaded?(map: mapboxgl.Map): void
+  onRemoved?(): void
 }
-// token necessario para acessar a api do mapbox
-const Map = ReactMapboxGl({
-  accessToken:
-    'pk.eyJ1IjoiZ2ZyZXNjaGlpIiwiYSI6ImNsMmRoNm52ZTAwZ2MzaXJ3MGJqd3I2NjQifQ.Gn1njvZQCcHTUt4MxbaVyA',
-})
 
-export default function MapboxMap() {
-  const [complaints, setComplaints] = useState<Complaint[] | null>([])
+function MapboxMap({
+  initialOptions = {},
+  onCreated,
+  onLoaded,
+  onRemoved,
+}: MapboxMapProps) {
+  const [map, setMap] = React.useState<mapboxgl.Map>()
 
-  const fetchedComplaint = [
-    { id: 0, latitude: -22.407, longitude: -47.562 },
-    { id: 1, latitude: -22.107, longitude: -47.562 },
-    { id: 2, latitude: -22.207, longitude: -47.562 },
-    { id: 3, latitude: -22.307, longitude: -47.562 },
-    { id: 4, latitude: -22.507, longitude: -47.562 },
-  ]
+  const mapNode = React.useRef(null)
 
-  useEffect(() => {
-    setComplaints(fetchedComplaint)
+  React.useEffect(() => {
+    const node = mapNode.current
+
+    if (typeof window === 'undefined' || node === null) return
+
+    const mapboxMap = new mapboxgl.Map({
+      container: node,
+      accessToken: 'pk.eyJ1IjoiZ2ZyZXNjaGlpIiwiYSI6ImNsMmRoNm52ZTAwZ2MzaXJ3MGJqd3I2NjQifQ.Gn1njvZQCcHTUt4MxbaVyA',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [-47.57, -22.41],
+      zoom: 9,
+      ...initialOptions,
+    })
+
+    setMap(mapboxMap)
+    if (onCreated) onCreated(mapboxMap)
+
+    if (onLoaded) mapboxMap.once('load', () => onLoaded(mapboxMap))
+
+    return () => {
+      mapboxMap.remove()
+      setMap(undefined)
+      if (onRemoved) onRemoved()
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const [viewport, setViewport] = React.useState({
-    width: '100vw',
-    height: '100vh',
-    latitude: -22.407,
-    longitude: -47.562,
-    zoom: [13],
-  })
-  return (
-    <Box>
-      <Map
-        style="mapbox://styles/mapbox/streets-v9"
-        center={[-47.5635479, -22.3934574]} // long and lat
-        containerStyle={{
-          height: '100vh',
-          width: '100vw',
-        }}
-        zoom={[13]}
-      >
-        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-          <Feature coordinates={[-47.5635479, -22.3934574]} />
-        </Layer>
-      </Map>
-    </Box>
-  )
+  return <div ref={mapNode} style={{ width: '100%', height: '100%' }} />
 }
 
-// eslint-disable-next-line no-lone-blocks
-{
-  /* {complaints.map(complaint => (
-            <Marker
-              key={complaint.id}
-              coordinates={[complaint.longitude, complaint.latitude]}
-              anchor="bottom"
-            >
-              <Popup
-                key={complaint.id}
-                coordinates={[complaint.longitude, complaint.latitude]}
-                anchor="bottom"
-              >
-                <div>
-                  <h3>Complaint - {complaint.id}</h3>
-                </div>
-              </Popup>
-            </Marker>
-          ))} */
-}
+export default MapboxMap
--- src/pages/complaints/new.tsx
+++ src/pages/complaints/new.tsx
@@ -1,9 +1,13 @@
-import NewComplaintForm from '../../components/Complaints/NewComplaintForm'
+import Layout from 'src/components/Layout'
+import NewComplaintForm from 'src/components/Complaints/NewComplaintForm'
+import WorldMapBox from 'src/components/WorldMapBox'
 
 export default function New() {
   return (
-    <div>
-      <NewComplaintForm />
-    </div>
+    <>
+      <Layout title="New Complaints">
+        <NewComplaintForm />
+      </Layout>
+    </>
   )
 }
